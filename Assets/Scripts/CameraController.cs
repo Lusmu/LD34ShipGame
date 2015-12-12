0c876bb9e497b71487609ef154a8c6bd2185eff4
Timo Moisio <timohmoisio@gmail.com>
2015-12-12 19:22:49 +0200

    Change camera mode

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -13,21 +13,29 @@ namespace IfelseMedia.GuideShip
         private float moveSpeed = 10;
 
         [SerializeField]
+        private float turnSpeed = 30;
+
+        [SerializeField]
         private float prediction = 5;
 
         void Update()
         {
             if (target != null)
             {
-                var targetPosition = target.position + offset;
+                var targetPosition = target.position + Vector3.up * offset.y - target.forward * offset.z;
+                var targetLookPosition = target.position;
 
                 var physics = target.GetComponent<Rigidbody>();
                 if (physics != null)
                 {
-                    targetPosition += physics.velocity * prediction; 
+                    targetPosition -= physics.velocity * prediction;
+                    targetLookPosition += physics.velocity * prediction;
                 }
 
                 transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * moveSpeed);
+
+                var lookRotation = Quaternion.LookRotation(targetLookPosition - transform.position);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, lookRotation, Time.deltaTime * turnSpeed);
             }
         }
     }
