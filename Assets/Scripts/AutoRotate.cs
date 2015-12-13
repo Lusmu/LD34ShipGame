867cf2070e9e1a2ab54af63e6241f6926f7d08c0
Timo Moisio <timohmoisio@gmail.com>
2015-12-13 10:44:34 +0200

    Harbour WIP

---
+++ Assets/Scripts/AutoRotate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoRotate : MonoBehaviour 
+{
+	public Vector3 speed = new Vector3(0, 90, 0);
+
+	void Update ()
+	{
+		transform.eulerAngles = speed * Time.time;
+	}
+}
