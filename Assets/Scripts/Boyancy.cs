1aa637ee8f5bb870d6c8bcf2f2c3f7cea0111d5f
Timo Moisio <timohmoisio@gmail.com>
2015-12-13 17:15:55 +0200

    Add wind

--- Assets/Scripts/Boyancy.cs
+++ Assets/Scripts/Boyancy.cs
@@ -7,7 +7,10 @@ namespace IfelseMedia.GuideShip
 	public class Boyancy : MonoBehaviour 
 	{
 		public float strength = 1.1f;
-		private Rigidbody physics;
+
+        public Vector3 wind;
+
+        private Rigidbody physics;
 
 		void Awake()
 		{
@@ -16,14 +19,13 @@ namespace IfelseMedia.GuideShip
 
 		void FixedUpdate () 
 		{
-			
 			var waterLevel = WaterManager.GetWaveHeight (transform.position);
 
 			var pos = transform.position;
 			var diff = Mathf.Clamp01(waterLevel - pos.y);
 			if (diff > 0)
 			{
-				physics.AddForce(Vector3.up * 2000 * diff * diff * Time.deltaTime, ForceMode.Acceleration);
+				physics.AddForce(wind + Vector3.up * 2000 * diff * diff * Time.deltaTime, ForceMode.Acceleration);
 			}
 		}
 
